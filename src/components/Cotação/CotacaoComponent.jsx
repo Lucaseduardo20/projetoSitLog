23539bff62a53ff71ce0e334b60bc89ababb5650
Lucas Eduardo <lucas.moraes@compila.com.br>
2023-06-20 09:01:02 -0300

    feat: Implementação da feature de cotação v2

--- src/components/Cotação/CotacaoComponent.jsx
+++ src/components/Cotação/CotacaoComponent.jsx
@@ -1,22 +1,43 @@
 import { useState } from "react";
 import { CotacaoArticle, CotacaoHeader, CotacaoContent, CotacaoForm, CotacaoProgress } from "./CotacaoComponentStyled";
+import { Line } from 'rc-progress';
 
 export default function CotacaoComponent() {
+    const [progress, setProgress] = useState(0);
+
+
+    const menageProgress = (value) => {
+        cpnsole.log(value)
+    }
     return (
-        <>
                 <CotacaoContent>
                     <CotacaoArticle>
                         <CotacaoHeader>
-
+                            <h2>COTAÇÃO</h2> <span>RÁPIDA</span>
+                            <p>Solicite sua cotação e fale com um de nossos consultores.</p>
                         </CotacaoHeader>
                         <CotacaoForm>
-
+                            <input type="text" class="styled-input" placeholder="Nome Completo" onBlur={e => menageProgress(e)}/>
+                            <input type="text" class="styled-input" placeholder="Telefone" onBlur={e => menageProgress(e.target.value)}/>
+                            <input type="text" class="styled-input" placeholder="E-mail" onBlur={e => menageProgress(e.target.value)}/>
+                            <input type="text" class="styled-input" placeholder="Empresa" onBlur={e => menageProgress(e.target.value)}/>
+                            <input type="text" class="styled-input" placeholder="CPF/CNPJ" onBlur={e => menageProgress(e.target.value)}/>
+                            <button id="submit"> ENVIAR</button>
                         </CotacaoForm>
-                        <CotacaoProgress>
-
-                        </CotacaoProgress>
+                    <CotacaoProgress>
+                        <span>{progress}%</span>
+                        <Line
+                            percent={progress}
+                            strokeWidth={5}
+                            trailWidth={5}
+                            strokeColor="#9E1E33"
+                            trailColor="#4f0f19"
+                            strokeLinecap="round"
+                            style={{ position: 'relative', width: '50%' }}
+                        >
+                        </Line>
+                    </CotacaoProgress>
                     </CotacaoArticle>
                 </CotacaoContent>
-        </>
     )
 }
--- src/components/Cotação/CotacaoComponentStyled.jsx
+++ src/components/Cotação/CotacaoComponentStyled.jsx
@@ -3,34 +3,112 @@ import styled from "styled-components";
 export const CotacaoContent = styled.section`
     width: 100%;
     height: 100vh;
-    border: 5px solid black;
     display: flex;
     justify-content: center;
     align-items: center;    
 `
 
 export const CotacaoArticle = styled.article `
-    width: 70%;
-    height: 80%;
-    border: 3px solid red;
+    width: 65%;
+    height: 70%;
     border-radius: 30px;
     display: flex;
     flex-direction: column;
     justify-content: space-around;
     align-items: center;
+    -webkit-box-shadow: 1px 2px 26px -2px rgba(0,0,0,1);
+    -moz-box-shadow: 1px 2px 26px -2px rgba(0,0,0,1);
+    box-shadow: 1px 2px 26px -2px rgba(0,0,0,1);
 `
 
 export const CotacaoHeader = styled.div `
-    width: 90%;
-    border: 1px solid blue;
+    width: 95%;
+    height: 25%;
+    padding: 20px 0 0 40px;
+
+    h2 {
+        display: inline;
+        font-size: 80px;
+        color: #3B3A3A;
+        font-family: Calibri;
+
+    }
+
+    span {
+        font-size: 80px;
+        font-style: italic;
+        color: #B30220;
+        font-family: Calibri;
+    }
+
+    p {
+        font-size: 23px;
+        margin: -5px     0 0 5px;
+        font-family: Calibri;
+        color: #000000;
+        letter-spacing: -1px;
+    }
 `
 
 export const CotacaoForm = styled.div `
-    width: 90%;
-    border: 1px solid green;
+    width: 95%;
+    height: 50%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+
+    >input {
+        width: 35%;
+        height: 50px;
+        border: 1pt solid #707070;
+        font-size: 26px;
+
+        ::placeholder {
+            color: #A2A2A2;
+            font-size: 26px;
+            font-family: Calibri;
+        }
+    }
+
+    >button {
+        width: 30%;
+        margin: 0 40px 0 0;
+        height: 50px;
+        border-radius: 40px;
+        background: #757575;
+        border: none;
+        transition: 0.3s;
+        color: #DFDFDF;
+        font-family: Calibri;
+        font-weight: bold;
+        font-size: 42px;
+        
+        :hover {
+            background: #b30220;
+            cursor: pointer;
+        }
+    }
+
+    .styled-input::placeholder {
+        position: absolute;
+        margin-left: 30px;
+    }
 `
 
 export const CotacaoProgress = styled.div `
-    width: 90%;
-    border: 1px solid red;
+    width: 95%;
+    height: 25%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    span {
+        position: absolute;
+        color: white;
+        font-family: Calibri;
+        font-weight: bold;
+        font-size: 25px;
+        z-index: 1;
+    }
 `
