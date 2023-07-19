e243cd723a3ad48642e31de32175415691f0a976
Lucas Eduardo <lucas.moraes@compila.com.br>
2023-07-18 21:52:37 -0300

    feat: feedback v2

--- src/components/Feedbacks/FeedbacksComponent.jsx
+++ src/components/Feedbacks/FeedbacksComponent.jsx
@@ -1,17 +1,121 @@
 import React from 'react'
 import { FeedbacksStyled } from './FeedbacksStyled'
+import { FaStar } from 'react-icons/fa'
 
 
 export default function Feedbacks () {
+
+    // const feedbacks = [
+    //     {
+    //         'nomeEmpresa': 'lucasdev',
+    //         'texto': 'muito bom seu trabalho',
+    //         'quantidade': 5
+    //     },
+    //     {
+    //         'nomeEmpresa': 'lucasdev',
+    //         'texto': 'muito bom seu trabalho',
+    //         'quantidade': 5
+    //     },
+    //     {
+    //         'nomeEmpresa': 'lucasdev',
+    //         'texto': 'muito bom seu trabalho',
+    //         'quantidade': 5
+    //     },
+    //     {
+    //         'nomeEmpresa': 'lucasdev',
+    //         'texto': 'muito bom seu trabalho',
+    //         'quantidade': 5
+    //     }
+    // ]
+
     return (
         <FeedbacksStyled>
             <h2>Avaliações</h2>
             <article>
-                <div>oi</div>
-                <div>oi</div>
-                <div>oi</div>
-                <div>oi</div>
+                <div>
+                    <div className="feedback-header">
+                        <div>.</div>
+                        <span>Nome do cliente</span>
+                    </div>
+                    <div className="feedback-body">
+                        <p>"Conseguiram construir uma solução que vai te economizar muito tempo, e você vai poder dedicar mais para sua aplicação."</p>
+                    </div>
+                    <div className="feedback-footer">
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                    </div>
+                </div>
+
+                <div> 
+                    <div className="feedback-header">
+                        <div>.</div>
+                        <span>Nome do cliente</span>
+                    </div>
+                    <div className="feedback-body">
+                        <p>"Conseguiram construir uma solução que vai te economizar muito tempo, e você vai poder dedicar mais para sua aplicação."</p>
+                    </div>
+                    <div className="feedback-footer">
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                    </div>
+                </div>
+
+                <div>
+                    <div className="feedback-header">
+                        <div>.</div>
+                        <span>Nome do cliente</span>
+                    </div>
+                    <div className="feedback-body">
+                        <p>"Conseguiram construir uma solução que vai te economizar muito tempo, e você vai poder dedicar mais para sua aplicação."</p>
+                    </div>
+                    <div className="feedback-footer">
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                    </div>
+                </div>
+
+                <div>
+                    <div className="feedback-header">
+                        <div>.</div>
+                        <span>Nome do cliente</span>
+                    </div>
+                    <div className="feedback-body">
+                        <p>"Conseguiram construir uma solução que vai te economizar muito tempo, e você vai poder dedicar mais para sua aplicação."</p>
+                    </div>
+                    <div className="feedback-footer">
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                    </div>
+                </div>              
             </article>
         </FeedbacksStyled>
     )
 }
+// {feedbacks.map(feedback => (
+//     <div>
+//         <div className="feedback-header">
+//             <img></img>
+//             <span>{feedback.nomeEmpresa}</span>
+//         </div>
+//         <div className="feedback-body">
+//             <p>{feedback.texto}</p>
+//         </div>
+//         <div className="feedback-footer">
+//             {/* {feedback.quantidade.map(estrela => (
+//                 <p>estrela</p>
+//             ))} */}
+//         </div>
+//     </div>
+// ))}
--- src/components/Feedbacks/FeedbacksStyled.jsx
+++ src/components/Feedbacks/FeedbacksStyled.jsx
@@ -2,8 +2,7 @@ import styled from 'styled-components'
 
 export const FeedbacksStyled = styled.section `
     width: 100%;
-    height: 800px;
-    border: 2px solid red;
+    height: 600px;
     display: flex;
     justify-content: space-evenly;
     align-items: center; 
@@ -17,18 +16,64 @@ export const FeedbacksStyled = styled.section `
     }
 
     article {
-        border: 5px solid black;
-        width: 60%;
-        height: 80%;
+        width: 50%;
+        height: 70%;
         display: flex;
         align-items: center;
         flex-wrap: wrap;
         justify-content: space-evenly;
 
-        div {
-            width: 40%;
-            height: 40%;
-            border: 2px solid red;
+        >div {
+            width: 42%;
+            height: 43%;
+            border-radius: 27px;
+            background-color: #F2F2F2DB;
+            box-shadow: #00000066 7px 7px 7px;
+            padding: 20px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+
+            .feedback-header{
+                width: 80%;
+                height: 35%;
+                display: flex;
+                align-items: center;
+
+                >div {
+                    width: 20%;
+                    height: 100%;
+                    border-radius: 50%;
+                    background-color: #5d5d5d;
+                }
+
+                span {
+                    font-size: 17px;
+                    font-weight: bold;
+                    margin: 0 0 0 10px;
+                    font-family: Calibri;
+                }
+            }
+
+            .feedback-body{
+
+                >p {
+                    font-family: Calibri;
+                    font-size: 16.8px;
+                }
+            }
+
+            .feedback-footer{
+                display: flex;
+                align-items: flex-end;
+                width: 40%;
+                justify-content: space-around;
+
+                >svg {
+                    fill: #ebce3d;
+                    width: 20px;
+                }
+            }
         }  
     }
    
