eea393b5ead54a74abb7d4df3353459e65d04f4c
Lucas Eduardo <lucaseduardo3528@hotmail.com>
2023-07-25 23:24:14 -0300

    feat: footer v2

--- src/components/Footer/FooterComponent.jsx
+++ src/components/Footer/FooterComponent.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FooterContainer, FooterContent, FooterInfos, FooterStreetView } from "./FooterStyled";
 import LogoBranco from '../../assets/img/logo-sitlog-branco.png'
+import { FaWhatsapp, FaFacebookF, FaInstagram, FaLinkedinIn, FaBars } from 'react-icons/fa';
 
 export const Footer = () => {
     return (
@@ -8,6 +9,18 @@ export const Footer = () => {
             <FooterContent>
                 <FooterInfos>
                     <img src={LogoBranco}></img>
+                    <div id="infos">
+                        <span>(011) 96683-6931</span>
+                        <span>Sitlog@sitlog2.com.br</span>
+                        <span>Rua Ouro Grosso, 1115 - Bairro Parque Peruche<br />São Paulo - SP, CEP 02555-000</span>
+                        <div id="icons">
+                            <FaInstagram />
+                            <FaFacebookF />
+                            <FaWhatsapp />
+                            <FaLinkedinIn />
+                        </div>
+
+                    </div>
                 </FooterInfos>
                 <FooterStreetView></FooterStreetView>
             </FooterContent>
--- src/components/Footer/FooterStyled.jsx
+++ src/components/Footer/FooterStyled.jsx
@@ -11,20 +11,47 @@ export const FooterContainer = styled.footer `
 export const FooterContent = styled.div `
     width: 80%;
     height: 75%;
-    border: 5px solid yellow;
+    // border: 5px solid yellow;
     margin: 30px 0 0 0;
     display: flex;
 `
 
 export const FooterInfos = styled.div `
     width: 50%;
-    border: 2px solid green;
+    // border: 2px solid green;
     display: flex;
     flex-direction: column;
     align-items: flex-start;
+    justify-content: space-evenly;
 
     >img {
-        width: 300px;
+        width: 200px;
+        margin: 0 0 15px 10px;
+    }
+
+    #infos {
+        display: flex;
+        flex-direction: column;
+
+        >span {
+            color: white;
+            font-family: Calibri;
+            margin: 3px 0;
+            font-size: 17px;
+        }
+
+        #icons {
+            svg {
+                fill: white;
+                width: 25px;
+                height: 20px;
+                transition: .5s;
+
+                :hover {
+                    fill: red;
+                }
+            }
+        }
     }
 `
 export const FooterStreetView = styled.div `
