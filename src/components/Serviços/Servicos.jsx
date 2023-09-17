5ffe0bef2bb8bdb521c0e62469a065b3b71980fe
Lucas Eduardo <lucas.moraes@compila.com.br>
2023-09-17 05:31:36 -0300

    feat: servicos v2

--- src/components/Serviços/Servicos.jsx
+++ src/components/Serviços/Servicos.jsx
@@ -1,7 +1,46 @@
 import { useState } from "react";
-import { ServicosStyled, ServicosHeader, ServicosContent } from "./ServicosStyled";
+import { ServicosStyled, ServicosHeader, ServicosContent, ServicoItem } from "./ServicosStyled";
+import { useEffect } from "react";
+import Expressa from './expressa.png'
+import Fracionada from './fracionada.png'
+import Especial from './especial.png'
 
 export default function ServicosComponent() {
+    const [data, setData] = useState([
+        {
+            'nome': 'Fracionada',
+            'isActive': true,
+            'imagem': Fracionada,
+            'texto': 'Em termos simples, a carga fracionada é um tipo de serviço no qual o embarcador paga somente pelo espaço no caminhão utilizado pela sua remessa. Dessa forma, o custo com o transporte é fracionado entre outras cargas e, portanto, diminuído.'
+        },
+        {
+            'nome': 'Especial',
+            'isActive': false,
+            'imagem': Especial,
+            'texto': 'São consideradas especiais mercadorias com necessidades específicas em relação à temperatura, umidade, que sejam frágeis, perecíveis ou muito pesadas. Medicamentos são ótimos exemplos de transporte de cargas especiais. Dessa maneira, não podem ser transportadas da forma padrão, exigindo um maior planejamento.'
+        },
+        {
+            'nome': 'expressa',
+            'isActive': false,
+            'imagem': Expressa,
+            'texto': 'Por definição, carga expressa é aquela que demanda alta prioridade, com entrega pontual e no menor tempo possível. A entrega expressa costuma se dar de ponta a ponta e, a depender da distância, envolve o modal aéreo.'
+        }
+    ]);
+ 
+    const HandleActivate = (servico, isActive) => {
+        const updatedData = data.map(item => {
+            if (item.nome === servico) {
+                return { ...item, isActive: isActive };
+            }
+            return item;
+        });
+
+        setData(updatedData)
+    }
+
+    console.log(data)
+
+
     return (
         <ServicosStyled>
             <ServicosHeader>
@@ -18,9 +57,18 @@ export default function ServicosComponent() {
                 <button>SAIBA MAIS</button>
             </ServicosHeader>
             <ServicosContent>
-                <div></div>
-                <div></div>
-                <div></div>
+                    {data.map((servico) => 
+                        <ServicoItem servico={servico} onClick={() => HandleActivate(servico.nome, true)} onMouseLeave={() => HandleActivate(servico.nome, false)} key={servico.nome} className={servico.nome}>
+                            <img style={{
+                                width: '60%',
+                            }} src={servico.imagem} alt="" />
+                            <span><strong>{`CARGA ${servico.nome.toUpperCase()}`}</strong></span>
+                            {servico.isActive && 
+                                <p>{servico.texto}</p>
+                            }
+                        </ServicoItem>
+                    )}
+                    {/* <img src={Expressa} alt="" /> */}
             </ServicosContent>
         </ServicosStyled>
     )
--- src/components/Serviços/ServicosStyled.jsx
+++ src/components/Serviços/ServicosStyled.jsx
@@ -3,12 +3,11 @@ import background from './servicos-background.png'
 
 export const ServicosStyled = styled.section`
     width: 100%;
-    height: 100vh;
+    height: 110vh;
     display: flex;
     flex-direction: column;
-    justify-content: space-around;
+    justify-content: space-between;
     align-items: center;
-    border: 5px solid black;
     background: url(${background}) no-repeat;
     background-size: 100%;
     background-position: center;
@@ -47,17 +46,59 @@ export const ServicosHeader = styled.div `
 `
 
 export const ServicosContent = styled.div `
-    border: 2px solid white;
     width: 50%;
     height: 200px;
     display: flex;
     align-items: center;
     justify-content: space-around;
+    margin: 0 0 200px 0;
+`
 
-    >div {
-        width: 200px;
-        height: 200px;
-        border-radius: 50%;
-        background-color: white;
+export const ServicoItem = styled.div `
+    width: ${props => props.servico.isActive ? '220px' : '200px'};
+    height: ${props => props.servico.isActive ? '570px' : '200px'};
+    border-radius: ${props => props.servico.isActive ? '0' : '50%'};
+    background-color: white;
+    display: flex;
+    justify-content: ${props => props.servico.isActive ? 'space-around' : 'center'};
+    align-items: center;
+    flex-direction: column;
+    padding: ${props => props.servico.isActive ? '15px' : '0'};
+
+    // transition: width 0.3s ease, height 0.4s ease, border-radius 0.3s ease, justify-content 0.3s ease, padding 0.3s ease;
+    transition: 
+    width 0.4s cubic-bezier(0.2, 0.8, 0.2, 1),
+    height 0.5s cubic-bezier(0.2, 0.8, 0.2, 1),
+    border-radius 0.3s cubic-bezier(0.2, 0.8, 0.2, 1),
+    justify-content 0.3s cubic-bezier(0.2, 0.8, 0.2, 1),
+    padding 0.2s cubic-bezier(0.2, 0.8, 0.2, 1);
+
+    >span {
+        font-size: 14px;
+        font-family: Calibri;
+        margin-top: 10px;
+    }
+
+    >p {
+        width: 80%;
+        height: 70%;
+        // letter-spacing: 1px;
+        font-size: ${props => props.servico.nome === 'Especial' ? '15px' : '17px'};
+        text-align: center;
+        line-height: 1.5;
+        font-family: Calibri;
+        margin: 30px 0 0 0;
+
+        transition: font-size 0.3s cubic-bezier(0.2, 0.8, 0.2, 1);
+    }
+
+    >img {
+        transition: transform 0.3s cubic-bezier(0.2, 0.8, 0.2, 1);
+        transform: scale(${props => props.servico.isActive ? '1.1' : '1'});
+    }
+
+    :hover {
+        cursor: pointer;
+        border: 2px solid red;
     }
 `
